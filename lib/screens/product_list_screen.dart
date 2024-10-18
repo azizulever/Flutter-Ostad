13be3bd693927dc81f76b93d6c3e55bd0f905e54
Azizul Islam <contact.azizulislam@gmail.com>
2024-10-18 22:06:14 +0600

    Practice Projects

--- lib/screens/product_list_screen.dart
+++ lib/screens/product_list_screen.dart
@@ -14,7 +14,7 @@ class _ProductListScreenState extends State<ProductListScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Product List"),
+        title: const Text("Product ist"),
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16.0),
